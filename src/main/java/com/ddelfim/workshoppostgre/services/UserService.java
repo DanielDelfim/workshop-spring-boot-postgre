a80af2a529163ed9a5cf2d28cb5ea49c6f0bafc1
Daniel Delfim <dmdelfim@gmail.com>
2019-04-05 20:00:03 -0300

    Insert users with post

--- src/main/java/com/ddelfim/workshoppostgre/services/UserService.java
+++ src/main/java/com/ddelfim/workshoppostgre/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ddelfim.workshoppostgre.domain.User;
+import com.ddelfim.workshoppostgre.dto.UserDTO;
 import com.ddelfim.workshoppostgre.repository.UserRepository;
 import com.ddelfim.workshoppostgre.services.exception.ObjectNotFoundException;
 
@@ -22,4 +23,13 @@ public class UserService {
 	public User findById(String id) {
 		return userRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object not found 2 "));
 	}
+	
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+	
 }
