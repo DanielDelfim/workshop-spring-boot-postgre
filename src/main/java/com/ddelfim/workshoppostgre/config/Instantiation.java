20168be3158260183fd6cac864b9ff24f4cca302
Daniel Delfim <dmdelfim@gmail.com>
2019-04-07 05:51:30 -0300

    Reference of posts

--- src/main/java/com/ddelfim/workshoppostgre/config/Instantiation.java
+++ src/main/java/com/ddelfim/workshoppostgre/config/Instantiation.java
@@ -1,6 +1,5 @@
 package com.ddelfim.workshoppostgre.config;
 
-
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.TimeZone;
@@ -16,37 +15,39 @@ import com.ddelfim.workshoppostgre.repository.PostRepository;
 import com.ddelfim.workshoppostgre.repository.UserRepository;
 
 @Configuration
-public class Instantiation implements CommandLineRunner{
+public class Instantiation implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private PostRepository postRepository;
-	
+
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-		
+
 		userRepository.deleteAll();
 		postRepository.deleteAll();
-		
+
 		User maria = new User(null, "maria", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "bob", "bob@gmail.com");
 		User daniel = new User(null, "daniel", "daniel@gmail.com");
-				
+
 		userRepository.saveAll(Arrays.asList(maria, alex, bob, daniel));
-		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços", new AuthorDTO(maria));
+
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços",
+				new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
-		
-		
+
 		postRepository.saveAll(Arrays.asList(post1, post2));
-		
+
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
+
 	}
-	
 
 }
