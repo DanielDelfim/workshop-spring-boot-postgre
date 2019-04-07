a794807ed4012ffb13c4b5e3889c8bace2b5149c
Daniel Delfim <dmdelfim@gmail.com>
2019-04-07 05:25:30 -0300

    Creating entity with user together

--- src/main/java/com/ddelfim/workshoppostgre/config/Instantiation.java
+++ src/main/java/com/ddelfim/workshoppostgre/config/Instantiation.java
@@ -1,13 +1,17 @@
 package com.ddelfim.workshoppostgre.config;
 
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.ddelfim.workshoppostgre.domain.Post;
 import com.ddelfim.workshoppostgre.domain.User;
+import com.ddelfim.workshoppostgre.repository.PostRepository;
 import com.ddelfim.workshoppostgre.repository.UserRepository;
 
 @Configuration
@@ -16,17 +20,28 @@ public class Instantiation implements CommandLineRunner{
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private PostRepository postRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "maria", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "bob", "bob@gmail.com");
 		User daniel = new User(null, "daniel", "daniel@gmail.com");
+				
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços", maria);
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob, daniel));
+		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 	}
 	
